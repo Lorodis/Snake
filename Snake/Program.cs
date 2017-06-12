d6f36f71cceb94483e2f1bf9845f46efb523f46f
Lorodis <dkgrafeve@gmail.com>
2017-06-12 13:21:05 +0700

    Completed

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,19 +10,11 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.SetBufferSize(120, 40);
             Console.CursorVisible = false;
 
-            HorizontalLine topLine = new HorizontalLine(0, 78, 0, '—');
-            HorizontalLine bottomLine = new HorizontalLine(0, 78, 24, '—');
-            VerticalLine leftLine = new VerticalLine(0, 0, 24, '|');
-            VerticalLine rightLine = new VerticalLine(78, 0, 24, '|');
-
-            topLine.Draw();
-            bottomLine.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
-
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+            
             Point p = new Point(7, 3, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
@@ -33,6 +25,11 @@ namespace Snake
             
             while(true)
             {
+                if(walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+
                 if(snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
@@ -51,6 +48,29 @@ namespace Snake
                     snake.HandleKey(key.Key);                    
                 }
             }
+
+            WriteGameOver();
+            Console.ReadLine();
+        }
+
+        static void WriteGameOver()
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+            WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+            yOffset++;
+            WriteText("Нажмите ENTER для выхода из игры.", xOffset - 2, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+
+        }
+
+        static void WriteText(String text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
         }
     }
 }
