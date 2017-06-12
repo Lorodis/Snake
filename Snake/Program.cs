6bd38090f3fb4896ec8e67b2e40ddf65cbe69659
Lorodis <dkgrafeve@gmail.com>
2017-06-12 12:23:20 +0700

    lesson 12

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,7 +10,8 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-           Console.SetBufferSize(120, 40);
+            Console.SetBufferSize(120, 40);
+            Console.CursorVisible = false;
 
             HorizontalLine topLine = new HorizontalLine(0, 78, 0, '—');
             HorizontalLine bottomLine = new HorizontalLine(0, 78, 24, '—');
@@ -24,18 +25,32 @@ namespace Snake
 
             Point p = new Point(7, 3, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
-
             snake.Draw();
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+            
+            while(true)
+            {
+                if(snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+                                
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);                    
+                }
+            }
         }
     }
 }
